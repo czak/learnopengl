38d5b00c363db9a624b337e2ce73f60d2c49dcb4
Łukasz Adamczak <lukasz@czak.pl>
2019-02-22 14:59:49 +0100

    Texture rendering w/ pixelated pacman.png

--- 04-pac1/basic_fragment.glsl
+++ 04-pac1/basic_fragment.glsl
@@ -1,7 +1,12 @@
 #version 330 core
+in vec3 ourColor;
+in vec2 TexCoord;
+
 out vec4 FragColor;
+
+uniform sampler2D texture1;
 
 void main()
 {
-  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+  FragColor = texture(texture1, TexCoord);
 }
--- 04-pac1/pac1.cpp
+++ 04-pac1/pac1.cpp
@@ -1,15 +1,18 @@
+#define STB_IMAGE_IMPLEMENTATION
 #include "../common/shader.hpp"
+#include "../common/stb_image.h"
 #include "../common/window.hpp"
 
 int main() {
-  Window win(640, 480, "Hello");
+  Window win(448, 576, "Hello");
   Shader shader("basic_vertex.glsl", "basic_fragment.glsl");
 
   float vertices[] = {
-      0.5f,  0.5f,  0.0f,  // top right
-      0.5f,  -0.5f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f,  // bottom left
-      -0.5f, 0.5f,  0.0f   // top left
+      // positions          // colors           // texture coords
+      1.0f,  1.0f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
+      1.0f,  -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+      -1.0f, 1.0f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
   };
 
   unsigned int indices[] = {
@@ -30,16 +33,49 @@ int main() {
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // teture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
 
+  // texture
+  unsigned int texture;
+
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  int width, height, nchannels;
+  unsigned char* data = stbi_load("pacman.png", &width, &height, &nchannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    // glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    fprintf(stderr, "Failed to load texture\n");
+  }
+  stbi_image_free(data);
+
   while (!win.shouldClose()) {
     glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glUniform1i(glGetUniformLocation(shader.id, "texture"), 0);
 
     glUseProgram(shader.id);
     glBindVertexArray(VAO);
