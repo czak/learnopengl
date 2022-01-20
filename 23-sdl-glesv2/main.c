f0c6e9b0e3390b61826ea3351d860b747ecab52b
czak <czak@earth>
2022-01-20 19:07:54 +0100

    Simple glesv2 example

---
+++ 23-sdl-glesv2/main.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <GLES2/gl2.h>
+
+char *vertex_shader_source =
+"attribute vec4 vPosition;\n"
+"void main()\n"
+"{\n"
+"  gl_Position = vPosition;\n"
+"}\n";
+
+char *fragment_shader_source =
+"void main()\n"
+"{\n"
+"  gl_FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
+
+static void checkCompileErrors(GLuint id) {
+  int success;
+  char infoLog[512];
+  glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(id, 512, NULL, infoLog);
+    fprintf(stderr, "Compile error: %s\n", infoLog);
+  }
+}
+
+static void checkLinkErrors(GLuint id) {
+  int success;
+  char infoLog[512];
+  glGetProgramiv(id, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(id, 512, NULL, infoLog);
+    fprintf(stderr, "Link error: %s\n", infoLog);
+  }
+}
+
+unsigned int loadShader(const char *source, GLenum type) {
+  unsigned int shader = glCreateShader(type);
+  glShaderSource(shader, 1, &source, NULL);
+  glCompileShader(shader);
+
+  checkCompileErrors(shader);
+
+  return shader;
+}
+
+int main() {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    exit(1);
+  }
+
+  SDL_Window* window = SDL_CreateWindow(
+      "sample",
+      10,
+      SDL_WINDOWPOS_UNDEFINED,
+      800,
+      600,
+      SDL_WINDOW_OPENGL);
+  if (!window) {
+    fprintf(stderr, "Couldn't create window: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+  context = SDL_GL_CreateContext(window);
+  if (!context) {
+    fprintf(stderr, "Couldn't create context: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  int attr;
+  SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &attr);
+  fprintf(stderr, "attr: %d\n", attr);
+
+  // --- SHADERS ---
+
+  unsigned int vertexShader = loadShader(vertex_shader_source, GL_VERTEX_SHADER);
+  unsigned int fragmentShader = loadShader(fragment_shader_source, GL_FRAGMENT_SHADER);
+
+  // --- PROGRAM ---
+
+  unsigned int program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+  checkLinkErrors(program);
+
+  glViewport(0, 0, 800, 600);
+
+  // Set up vertices
+  float vertices[] = {
+    0.5f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0
+  };
+
+  unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3,
+  };
+
+  glUseProgram(program);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+  glEnableVertexAttribArray(0);
+
+  int running = 1;
+  int frames = 0;
+  while (running) {
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_KEYDOWN:
+          if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+            running = 0;
+          break;
+      }
+    }
+
+    glClearColor(0.2f, 0.5f, 0.3f, 0.3f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* glDrawArrays(GL_TRIANGLES, 0, 3); */
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices);
+
+    SDL_GL_SwapWindow(window);
+
+    ++frames;
+  }
+
+  fprintf(stderr, "frames: %d\n", frames);
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
